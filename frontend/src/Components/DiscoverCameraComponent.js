c55b9c5f87f5942ab766744727028a510f3d4a7a
Siddhi Powar <siddhiofficial22@gmail.com>
2023-09-08 16:32:58 -0700

    Discover cameras modal added

--- frontend/src/Components/DiscoverCameraComponent.js
+++ frontend/src/Components/DiscoverCameraComponent.js
@@ -1,11 +1,12 @@
 import React, {useState} from 'react';
-import { Button, Select } from 'antd';
+import { Button, Select, Modal } from 'antd';
 
 
 export default function DiscoverCameraComponent() {
     const [cameras, setCameras] = useState([]);
     const [showCameraList, setShowCameraList] = useState(false);
-    const [cameraConnected, setCameraConnected] = useState(false)
+    const [cameraConnected, setCameraConnected] = useState(false);
+    const [isModalVisible, setIsModalVisible] = useState(false);
 
     const camerasList = [
         {
@@ -27,6 +28,19 @@ export default function DiscoverCameraComponent() {
         console.log(e, "value")
         setSelectedCamera(e.target.value)
     }
+
+    const handleDiscoverCameras = async () => {
+        setIsModalVisible(true);
+        try {
+          // Perform the camera fetching logic here using fetchCameras
+          await fetchCameras();
+        } catch (error) {
+          // Handle errors if necessary
+          console.error('Error fetching cameras:', error);
+        }
+      }
+
+      
     //fetch the list of available cameras
     const fetchCameras = async () => {
         try {
@@ -66,52 +80,28 @@ export default function DiscoverCameraComponent() {
         setCameraConnected(true)
     }
     return (
-        <div style={{paddingTop: 16}}>
-            <h3>Connect to a camera</h3>
-            <Button type="primary" onClick={fetchCameras} >Find Cameras</Button>
-            {showCameraList && (
-                <div>
-                    {/* <h3>Available Cameras:</h3> */}
-                    {/* <ul>
-                        {cameras.map((camera => (
-                            <li key={(camera['cam_serial'])}>
-                                {camera['cam_serial']} : {camera['cam_info']}
-                            </li>
-                        )))}
-                    </ul> */}
-                    
-                    {/* <select
-                        className="form-control"
-                        value={selectedCamera["cam_serial"] || null}
-                        onChange={(e) => handleCameraChange(e)}
-                    >
-                        {camerasList.map((camera) => {
-                            return <option
-                            value={camera["cam_serial"]}
-                            selected={selectedCamera["cam_serial"] === camera["cam_serial"]}
-                            >
-                            {camera["cam_info"]}
-                            </option>
-                        })}
-                    </select> */}
-
-                    <Select
-                        defaultValue={camerasList[0]}
-                        style={{
-                            width: 300,
-                            marginTop: 16,
-                            marginRight: 8
-                        }}
-                        // loading
-                        options={camerasList}
-                        onChange={handleCameraChange}
-                    />
-                    <Button type="primary" onClick={connectSelectedCamera}>Connect</Button>
-                    <br />
-                    {cameraConnected && <b>Camera Connected</b>}
-                </div>
-            )}
+    <div style={{ paddingTop: 16 }}>
+        <div style={{ paddingTop: '16px' }}>
+            <h4>Connect to a camera</h4>
+            <Button onClick={handleDiscoverCameras}>Find Cameras</Button>
         </div>
+      <Modal
+        title="Available Cameras"
+        visible={isModalVisible}
+        onCancel={() => setIsModalVisible(false)}
+        onOk={connectSelectedCamera}
+      >
+        <Select
+          style={{
+            width: '100%',
+          }}
+          options={camerasList}
+          onChange={handleCameraChange}
+          value={selectedCamera}
+        />
+      </Modal>
+      {cameraConnected && <b>Camera Connected</b>}
+    </div>
     )
     
 };
