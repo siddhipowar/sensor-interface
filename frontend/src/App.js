bc9fcd8102511a96508f5ddfc64b1739258448aa
Siddhi Powar <siddhiofficial22@gmail.com>
2023-10-12 12:49:42 -0700

    Disconnect camera function updated in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import StreamOptionModal from './Modals/StreamOptionModal'
 import CameraSettingsModal from './Modals/CameraSettingsModal'
 import PointCloudRenderer from './Modals/PointCloudRenderer';
 import { Button, message } from 'antd';
+import axios from 'axios';
 
 const App = () => {
     const [OpenDiscoverCamModal, setOpenDiscoverCamModal] = useState(false)
@@ -51,24 +52,22 @@ const App = () => {
     // Function to disconnect from camera when stream option modal is closed
     const disconnectFromCamera = async () => {
         try {
-            const response = await fetch('http://localhost:8001/disconnect-camera', {
-                method: 'POST',
+            const response = await axios.post('http://localhost:8001/disconnect-camera', {
                 headers: {
                     'Content-Type': 'application/json',
                 },
             });
-            if (response.ok) {
-                const data = await response.json();
-                console.log(data.message);
-            }
-            else {
+    
+            if (response.status === 200) {
+                console.log(response.data.message);
+            } else {
                 console.error('Failed to disconnect from the camera');
             }
         } catch (error) {
             console.error('An error occurred', error);
         }
     };
-
+    
     return (
         <>
 
